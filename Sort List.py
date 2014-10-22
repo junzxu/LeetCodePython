96e118dd4ac14ef94be0182d3982e647987e3f76
junzxu <xujunzhe1022@gmail.com>
2014-10-21 23:24:31 -0400

    changed to a merge sort solution

--- Sort List.py
+++ Sort List.py
@@ -7,17 +7,56 @@
 class Solution:
     # @param head, a ListNode
     # @return a ListNode
-    def sortList(self, head):
-        if head == None:
+    def sortList(self,head):
+        if not head:
             return None
-        tmp = []
-        current = head
-        while current != None:
-            tmp.append(current.val)
-            current = current.next
-        tmp.sort()
-        current = head
-        for i in range(0,len(tmp)):
-            current.val = tmp[i]
-            current = current.next
+        head = self.sort(head)
         return head
+        
+    def sort(self,head):
+        #given a head node, split it into two part and sort them recursively, finally merge them
+        if head.next == None:
+            #when the list has only one node
+            return head
+        slow = fast = head
+        while fast != None and fast.next != None:
+            slow = slow.next
+            fast = fast.next.next
+        
+        mid = head.next
+        if slow.next != None:
+            mid = slow.next
+            slow.next = None
+        else:
+            #only two nodes in the list
+            head.next = None
+        l = self.sort(head)
+        r = self.sort(mid)
+        
+        #merge two sorted list
+        start = l if l.val < r.val else r
+        if l.val < r.val:
+            l = l.next
+        else:
+            r = r.next
+        cur = start
+        while l and r:
+            if l.val < r.val:
+                cur.next = l
+                cur = cur.next
+                l = l.next
+            else:
+                cur.next = r
+                cur = cur.next
+                r = r.next
+        while l:
+            cur.next = l
+            cur = cur.next
+            l = l.next
+        while r:
+            cur.next = r
+            cur = cur.next
+            r = r.next
+            
+        return start
+        
