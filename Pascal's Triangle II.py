1a4662e0a0abc6d0f4fb3610165d714ae3c6cf43
junzxu <xujunzhe1022@gmail.com>
2014-10-25 23:07:04 -0400

    changed a in place method

--- Pascal's Triangle II.py
+++ Pascal's Triangle II.py
@@ -1,9 +1,9 @@
 class Solution:
-    # @return a list of integers
-    def getRow(self, rowIndex):
-        values = [1 for i in range(0, rowIndex+1)] #we only need the bottom row 
-        for row in range(2, rowIndex+1):
-            tmp = [elem for elem in values]
-            for j in range(1, row):
-                values[j] = tmp[j-1] + tmp[j]
-        return values
+	# @return a list of integers
+	def getRow(self, rowIndex):
+		values = [1 for i in range(0, rowIndex+1)] #we only need the bottom row 
+		for row in range(1, rowIndex+1):
+			values[row] = 1
+			for col in range(row-1, 0, -1):
+				values[col] = values[col-1] + values[col]
+		return values
