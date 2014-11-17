f7920d6a167f2611cb7a0bb75712f9c03cd03c75
junzxu <xujunzhe1022@gmail.com>
2014-11-16 23:54:22 -0500

    O(N) solution

--- Next Permutation.py
+++ Next Permutation.py
@@ -4,30 +4,14 @@ class Solution:
     def nextPermutation(self, num):
         if not num:
             return []
-        if len(num) == 1:
-            return num
-        if len(num) == 2:
-            num[0],num[1] = num[1],num[0]
-            return num
-        
-        num[1:] = self.nextPermutation(num[1:])
-        if self.increasing(num[1:]):
-            k = 1
-            while k<len(num) and num[k] <= num[0]:
-                k += 1
-            if k != len(num):
-                #replace the first digit with next larger digit
-                num[0],num[k] = num[k],num[0]
-            else:
-                #put the first digit to the end
-                val = num.pop(0)
-                num.append(val)
-            return num
-        else:
-            return num
-    
-    def increasing(self, li):
-        for i in range(0,len(li)-1):
-            if li[i]>li[i+1]:
-                return False
-        return True
+    	i = j = -1
+    	for k in range(0,len(num)-1):
+    		if num[k] < num[k+1]:
+    			i = k
+    	if i == -1: return num[::-1]
+    	for k in range(i+1, len(num)):
+    		if num[i] < num[k]:
+    			j = k
+    	num[i], num[j] = num[j], num[i]
+    	next = num[:i+1] + sorted(num[i+1:])
+    	return next
