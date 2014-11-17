0cc538fdf6c25bf899d0a19c3e67c5f367038c29
junzxu <xujunzhe1022@gmail.com>
2014-11-16 23:54:15 -0500

    shortened

--- 3Sum.py
+++ 3Sum.py
@@ -1,28 +1,19 @@
 class Solution:
     # @return a list of lists of length 3, [[val1,val2,val3]]
     def threeSum(self, num):
-        if not num:
-            return []
+        if not num: return []
         res = []
-        visited = []
-        cache = {}
         num.sort()
-        for i in range(0,len(num)):
-            if num[i] in visited:
-                #we cover all pairs with num[i] as first value before
-                continue
-            if num[i] > 0:
-                # all the values will be greater than 0 afterwards 
-                break
+        for i,v1 in enumerate(num):
+            if v1 > 0: break
+            t = -v1
+            if i>0 and v1 == num[i-1]: continue
             cache = {}
-            duplicates = [] #deal with duplicate pairs
-            visited.append(num[i])
-            for j in range(i+1,len(num)):
-                if cache.has_key(num[j]) and num[j] not in duplicates:
-                    pair = cache[num[j]] + [num[j]]
-                    res.append(pair)
-                    duplicates.append(num[j])
+            for j,v2 in enumerate(num[i+1:]):
+                if not cache.has_key(v2):
+                    cache[t-v2] = 1
+                elif cache[v2] > 1: continue
                 else:
-                    cache[-num[i]-num[j]] = [num[i],num[j]]
-                    
+                    res.append([v1,t-v2,v2])
+                    cache[v2] += 1
         return res
