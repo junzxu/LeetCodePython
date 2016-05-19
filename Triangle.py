7bb989b64eb8c3ef0123b3bbfd41876eb1437a73
Junzhe Xu <xujunzhe1022@gmail.com>
2016-05-18 21:23:45 -0700

    Update Triangle.py
    Simplify

--- Triangle.py
+++ Triangle.py
@@ -2,12 +2,13 @@ class Solution:
     # @param triangle, a list of lists of integers
     # @return an integer
     def minimumTotal(self, triangle):
-        layer = len(triangle)
-        minpath = [0]*layer
-        for i, elem in enumerate(triangle[-1]):
-            minpath[i] = elem
-        for k in range(layer-2,-1,-1):
-            #from last row to first row
-            for i in range(0,k+1):
-                minpath[i] = min( minpath[i], minpath[i+1]) + triangle[k][i] #sum at current row should be current value + minimum of two neighbor value at previous row
-        return minpath[0]
+        """
+        :type triangle: List[List[int]]
+        :rtype: int
+        """
+        if len(triangle) == 1: return triangle[0][0]
+        
+        for i in range(len(triangle)-2, -1,-1):
+    	    for j in range(0, i+1):
+    		    triangle[i][j] += min(triangle[i+1][j], triangle[i+1][j+1])
+        return triangle[0][0]
