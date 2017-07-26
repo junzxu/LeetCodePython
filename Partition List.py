2d411d228e509bb72c1eca229b2c83fca884379f
Junzhe Xu <xujunzhe1022@gmail.com>
2017-07-25 22:05:20 -0700

    shorter answer

--- Partition List.py
+++ Partition List.py
@@ -9,34 +9,20 @@ class Solution:
     # @param x, an integer
     # @return a ListNode
     def partition(self, head, x):
-        if head is None:
-            return None
-            
-        l = g = None # define two pointer l for values less than x, g for values greater or equal than x
-        l_start = g_start = None  # remember the start location of l and g
+        lessHead = ListNode(0)
+        greaterHead = ListNode(0)
+        less = lessHead
+        great = greaterHead
         cur = head
-        
         while cur:
             if cur.val < x:
-                if not l:
-                    l = cur
-                    l_start = l
-                else:
-                    l.next = cur
-                    l = l.next
+                less.next = cur
+                less = less.next
             else:
-                if not g:
-                    g = cur
-                    g_start = cur
-                else:
-                    g.next = cur
-                    g = g.next
+                great.next = cur
+                great = great.next
             cur = cur.next
-
-        if g:
-            g.next = None
-        if l:
-            l.next = g_start
-            return l_start
-        else:
-            return g_start
+        
+        less.next = greaterHead.next
+        great.next = None
+        return lessHead.next
